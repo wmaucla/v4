f90c509a0be0b22a6696ca48b04c6e4c177ad820
wmaucla <zhengweima@gmail.com>
2021-12-12 20:46:40 -0500

    fix: removing the color scheme options

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -82,8 +82,6 @@ const StyledPic = styled.div`
     .img {
       position: relative;
       border-radius: var(--border-radius);
-      mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
       transition: var(--transition);
     }
 
